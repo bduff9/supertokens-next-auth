b25a8f87198eb415e58240738c3507618790e4d2
Brian Duffey <bduff9@gmail.com>
2023-04-21 08:53:16 -0500

    Auth changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,18 @@
 import { NextUIProvider } from '@nextui-org/react';
-
-function MyApp({ Component, pageProps }) {
-  return (
-    <NextUIProvider>
-      <Component {...pageProps} />
-    </NextUIProvider>
-  );
+import React from 'react';
+import SuperTokensReact, { SuperTokensWrapper } from 'supertokens-auth-react';
+import { frontendConfig } from '../config/frontendConfig';
+if (typeof window !== 'undefined') {
+	// we only want to call this init function on the frontend, so we check typeof window !== 'undefined'
+	SuperTokensReact.init(frontendConfig());
 }
-
+function MyApp({ Component, pageProps }) {
+	return (
+		<SuperTokensWrapper>
+			<NextUIProvider>
+				<Component {...pageProps} />
+			</NextUIProvider>
+		</SuperTokensWrapper>
+	);
+}
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,15 @@
 import { Card, Grid, Row, Text, Navbar, Button, Link } from '@nextui-org/react';
+import { SessionAuth } from 'supertokens-auth-react/recipe/session';
+import { signOut } from 'supertokens-auth-react/recipe/emailpassword';
+import { useState } from 'react';
 
 export default function Home() {
+  const [isloggedIn, setIsLoggedIn] = useState(null);
+	async function onLogout() {
+		await signOut();
+		setIsLoggedIn;
+		window.location.href = '/';
+	}
   const list = [
     {
       title: 'Orange',
@@ -45,61 +54,66 @@ export default function Home() {
   ];
 
   return (
-    <>
-      <Navbar isBordered variant='floating'>
-        <Navbar.Brand>
-          <Text b color='inherit' hideIn='xs'>
-            Test App
-          </Text>
-        </Navbar.Brand>
-        <Navbar.Content hideIn='xs' variant='highlight-rounded'>
-          <Navbar.Link href='#'>Features</Navbar.Link>
-          <Navbar.Link isActive href='#'>
-            Customers
-          </Navbar.Link>
-          <Navbar.Link href='#'>Pricing</Navbar.Link>
-          <Navbar.Link href='#'>Company</Navbar.Link>
-        </Navbar.Content>
-        <Navbar.Content>
-          <Navbar.Item>
-            <Button auto flat as={Link} href='http://localhost:3000/auth'>
-              Login
-            </Button>
-          </Navbar.Item>
-        </Navbar.Content>
-      </Navbar>
+		<SessionAuth>
+			<Navbar isBordered variant="floating">
+				<Navbar.Brand>
+					<Text b color="inherit" hideIn="xs">
+						Test App
+					</Text>
+				</Navbar.Brand>
+				<Navbar.Content hideIn="xs" variant="highlight-rounded">
+					<Navbar.Link href="#">Features</Navbar.Link>
+					<Navbar.Link isActive href="#">
+						Customers
+					</Navbar.Link>
+					<Navbar.Link href="#">Pricing</Navbar.Link>
+					<Navbar.Link href="#">Company</Navbar.Link>
+				</Navbar.Content>
+				<Navbar.Content>
+					<Navbar.Item>
+						{isloggedIn ? (
+							<Button auto flat as={Link} href="/auth">
+								Login
+							</Button>
+						) : (
+							<Button auto flat as={Link} onPress={onLogout}>
+								Sign Out
+							</Button>
+						)}
+					</Navbar.Item>
+				</Navbar.Content>
+			</Navbar>
 
-      <Grid.Container gap={2} justify='center'>
-        {list.map((item, index) => (
-          <Grid xs={6} sm={2.5} key={index}>
-            <Card isPressable>
-              <Card.Body css={{ p: 0 }}>
-                <Card.Image
-                  src={'https://nextui.org' + item.img}
-                  objectFit='cover'
-                  width='100%'
-                  height={140}
-                  alt={item.title}
-                />
-              </Card.Body>
-              <Card.Footer css={{ justifyItems: 'flex-start' }}>
-                <Row wrap='wrap' justify='space-between' align='center'>
-                  <Text b>{item.title}</Text>
-                  <Text
-                    css={{
-                      color: '$accents7',
-                      fontWeight: '$semibold',
-                      fontSize: '$sm',
-                    }}
-                  >
-                    {item.price}
-                  </Text>
-                </Row>
-              </Card.Footer>
-            </Card>
-          </Grid>
-        ))}
-      </Grid.Container>
-    </>
-  );
+			<Grid.Container gap={2} justify="center">
+				{list.map((item, index) => (
+					<Grid xs={6} sm={2.5} key={index}>
+						<Card isPressable>
+							<Card.Body css={{ p: 0 }}>
+								<Card.Image
+									src={'https://nextui.org' + item.img}
+									objectFit="cover"
+									width="100%"
+									height={140}
+									alt={item.title}
+								/>
+							</Card.Body>
+							<Card.Footer css={{ justifyItems: 'flex-start' }}>
+								<Row wrap="wrap" justify="space-between" align="center">
+									<Text b>{item.title}</Text>
+									<Text
+										css={{
+											color: '$accents7',
+											fontWeight: '$semibold',
+											fontSize: '$sm',
+										}}>
+										{item.price}
+									</Text>
+								</Row>
+							</Card.Footer>
+						</Card>
+					</Grid>
+				))}
+			</Grid.Container>
+		</SessionAuth>
+	);
 }
